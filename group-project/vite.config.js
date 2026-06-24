12170951f5ea2aa9925ea2d471d521b9cb6aadfc
VAIBHAV RAJ <vaibhavraj9dmanan@gmail.com>
2026-06-24 19:35:31 +0530

    WEB

--- group-project/vite.config.js
+++ group-project/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import React from "react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My React Project</h1>
+      <p>This is my first React application.</p>
+
+      <button
+        onClick={() => alert("Hello BCA Student!")}
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          cursor: "pointer"
+        }}
+      >
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
